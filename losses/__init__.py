e9368f9a1605ba6f3fb3b50b2559530a3d6bbb60
colorfulgreen <colorfulgreen@gmail.com>
2020-12-09 19:16:31 +0800

    automask & smoothness

---
+++ losses/__init__.py
@@ -0,0 +1,17 @@
+import torch
+
+def get_smooth_loss(disp, img):
+    """Computes the smoothness loss for a disparity image
+    The color image is used for edge-aware smoothness
+    """
+    grad_disp_x = torch.abs(disp[:, :, :, :-1] - disp[:, :, :, 1:])
+    grad_disp_y = torch.abs(disp[:, :, :-1, :] - disp[:, :, 1:, :])
+
+    grad_img_x = torch.mean(torch.abs(img[:, :, :, :-1] - img[:, :, :, 1:]), 1, keepdim=True)
+    grad_img_y = torch.mean(torch.abs(img[:, :, :-1, :] - img[:, :, 1:, :]), 1, keepdim=True)
+
+    grad_disp_x *= torch.exp(-grad_img_x)
+    grad_disp_y *= torch.exp(-grad_img_y)
+
+    return grad_disp_x.mean() + grad_disp_y.mean()
+
--- train.py
+++ train.py
@@ -11,6 +11,7 @@ from nets.depth_decoder import DepthDecoder
 from nets.pose_decoder import PoseDecoder
 from losses.photometric_reconstruction_loss import BackprojectDepth, Project3D, pose_vec2mat
 from losses.ssim import SSIM
+from losses import get_smooth_loss
 from tensorboardX import SummaryWriter
 
 
@@ -88,31 +89,55 @@ class Train(object):
         total_loss = 0
         for scale in self.SCALES:
             disp = disparities[scale]
+
             reprojection_loss = self.photometric_reconstruction_loss(tgt_img, ref_imgs, intrinsics, disp, poses)
-            total_loss += reprojection_loss
+
+            mean_disp = disp.mean(2, True).mean(3, True)
+            norm_disp = disp / (mean_disp + 1e-7)
+            b, _, h, w = disp.shape
+            scaled_tgt_img = F.interpolate(tgt_img, (h, w), mode='area')
+            smooth_loss = get_smooth_loss(norm_disp, scaled_tgt_img)
+
+            total_loss += reprojection_loss + smooth_loss / (2**scale) # TODO 2**scale
         return total_loss / len(self.SCALES)
 
-
     def photometric_reconstruction_loss(self, tgt_img, ref_imgs, intrinsics, disp, poses):
-        if disp.shape != tgt_img.shape:
+        if disp.shape != tgt_img.shape: # NOTE upsample the lower resolution depth map
             disp = F.interpolate(disp, [self.H, self.W], mode='bilinear', align_corners=False)
-        losses = []
         depth = 1 / disp # TODO depth
-        cam_coords = self.backproject_depth(depth, intrinsics.inverse())
 
+        reprojection_losses = []
+        cam_coords = self.backproject_depth(depth, intrinsics.inverse())
         for i, ref_img in enumerate(ref_imgs):
             pose = poses[:, i]     # [B, 6]
             T = pose_vec2mat(pose)
             pixel_coords = self.project_3d(cam_coords, intrinsics, T)
             projected_img = F.grid_sample(tgt_img, pixel_coords, padding_mode='zeros', align_corners=True)
-            abs_diff = torch.abs(tgt_img - projected_img)
-            l1_loss = abs_diff.mean(1, True) # [12, 1, 192, 640]
-            ssim_loss = self.ssim(projected_img, tgt_img)
-            reprojection_loss = 0.85 * ssim_loss + 0.15 * l1_loss
-            losses.append(reprojection_loss)
-        min_loss, _ = torch.min(torch.cat(losses, dim=1), dim=1)
-        # import pdb; pdb.set_trace()
-        return min_loss.mean()
+            reprojection_losses.append(self.appearance_similarity(tgt_img, projected_img))
+        reprojection_losses = torch.cat(reprojection_losses, dim=1)
+
+        # compute reprojection error of the original for auto-masking
+        identity_reprojection_losses = []
+        for i, ref_img in enumerate(ref_imgs):
+            identity_reprojection_losses.append(self.appearance_similarity(tgt_img, ref_img))
+        identity_reprojection_losses = torch.cat(identity_reprojection_losses, dim=1)
+        identity_reprojection_losses += torch.randn(identity_reprojection_losses.shape).cuda() * 0.00001 # add random numbe r to break ties?
+
+        # auto-masking that fillters out pixels which do not change appearance from one frame to the next
+        combined = torch.cat((identity_reprojection_losses, reprojection_losses), dim=1)
+        min_reprojection_losses, min_idxs = torch.min(combined, dim=1)
+        automask = (min_idxs > identity_reprojection_losses.shape[1] - 1).float()
+
+        loss = (automask * min_reprojection_losses).mean()  # NOTE per-pixel minimum reprojection loss
+
+        return loss
+
+    def appearance_similarity(self, tgt_img, projected_img):
+        abs_diff = torch.abs(tgt_img - projected_img)
+        l1_loss = abs_diff.mean(1, True) # [12, 1, 192, 640]
+        ssim_loss = self.ssim(projected_img, tgt_img)
+        similarity = 0.85 * ssim_loss + 0.15 * l1_loss
+        return similarity
 
     def save_model(self):
         folder = os.path.join(BASE_DIR, 'runs', 'models')
